c2e7bbec010c34b31e14a5d6382bfc34eb16576c
Dmytro Krupskyi <krupskydima@gmail.com>
2022-07-18 18:41:07 +0300

    Initial commit

---
+++ AnimalFacts/AnimalFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalFactsApp.swift
+//  AnimalFacts
+//
+//  Created by Dmytro Krupskyi on 18.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimalFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
